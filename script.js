80d18cbacec409e097f8a102b4464c57a21a101e
Planetsandstars <alic177749@gmail.com>
2026-08-19 00:04:56 -0500

    trying to add the footer to a js so it can be easily repeatable. edited the prof and personality views on index

--- script.js
+++ script.js
@@ -26,3 +26,9 @@ toggle.addEventListener("click", () => {
     }
 
 });
+
+fetch("footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  });
